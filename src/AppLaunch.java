02adb0c2fe3e0281627d97fdb9a825be9732eba4
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-09-05 09:24:37 +0900

    add app

---
+++ src/AppLaunch.java
@@ -0,0 +1,9 @@
+import model.Android;
+
+public class AppLaunch {
+
+	public static void main(String[] args) {
+		Android android = new Android();
+	}
+
+}
--- src/model/Android.java
+++ src/model/Android.java
@@ -2,4 +2,8 @@ package model;
 
 public class Android extends Device {
 
+	public Android() {
+		System.out.println("Hello, Android");
+	}
+
 }
