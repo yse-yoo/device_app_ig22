df882936db82fd5964a3f1ded907751ad60fc4dd
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-09-05 09:44:33 +0900

    add getter/setter

--- src/AppLaunch.java
+++ src/AppLaunch.java
@@ -1,9 +1,12 @@
 import model.Android;
+import model.Camera;
 
 public class AppLaunch {
 
 	public static void main(String[] args) {
 		Android android = new Android();
+		
+		Camera camera = new Camera();
 	}
 
 }
--- src/model/Software.java
+++ src/model/Software.java
@@ -2,4 +2,17 @@ package model;
 
 public class Software {
 
+	private String name;
+
+	// getter
+	public String getName() {
+		return name;
+	}
+
+	// setter
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
 }
